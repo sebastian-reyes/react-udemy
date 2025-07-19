db5dbb1fe9cd018fae77991e9522dcce1337b366
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-07-19 03:04:02 -0500

    base js: const, let

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+const nombre = 'Sebastián';
+const apellido = 'Reyes';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  const nombre = 'Peter';
+  let valorDado = 6;
+  console.log(valorDado, nombre);
+}
+
+console.log(valorDado);
